c2945dd56bad14661dbd0b947d46f7d4cdc084ee
hamedakheraty-svg <hamed.akheraty@gmail.com>
2026-09-02 00:19:07 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = "damdar-app-v1";
+const CACHE_NAME = "damdar-app-v2";
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
   "./icon-192.png",
-  "./icon-512.png"
+  "./icon-512.png",
+  "./xlsx.full.min.js"
 ];
 
 self.addEventListener("install", (event) => {
